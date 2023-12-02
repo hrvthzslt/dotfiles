ee19c2930b7e3e7c2080fe63b74ad5a45efa9912
Zsolt Horváth <hrvth.7slt@gmail.com>
2023-12-02 18:48:35 +0100

    nia

--- setup/nix.sh
+++ setup/nix.sh
@@ -2,56 +2,56 @@
 
 export NIXPKGS_ALLOW_UNFREE=1
 
-install_packages() {
+nia() {
   for package in "$@"; do
     nix-env -iA nixpkgs."$package"
   done
 }
 
 # Utilities
-install_packages fzf ripgrep eza fd sd entr act gtop imagemagick translate-shell
+nia fzf ripgrep eza fd sd entr act gtop imagemagick translate-shell
 
 # Dev tools
-install_packages act hurl lazydocker lazygit
+nia act hurl lazydocker lazygit
 
 # Workflow
-install_packages tmuxinator neovim starship
+nia tmuxinator neovim starship
 
 # Makeup
-install_packages starship dwt1-shell-color-scripts
+nia starship dwt1-shell-color-scripts
 
 # Interweb
-install_packages ddgr w3m links2
+nia ddgr w3m links2
 
 # Database
-install_packages mysql mycli redis iredis nodePackages.sql-formatter
+nia mysql mycli redis iredis nodePackages.sql-formatter
 
 # C
-install_packages gccgo
+nia gccgo
 
 # Shell
-install_packages shellcheck shfmt nodePackages.bash-language-server
+nia shellcheck shfmt nodePackages.bash-language-server
 
 # Go
-install_packages go gopls
+nia go gopls
 
 # PHP
-install_packages php82 php82Packages.php-cs-fixer php82Packages.phpstan phpactor nodePackages.intelephense
+nia php82 php82Packages.php-cs-fixer php82Packages.phpstan phpactor nodePackages.intelephense
 
 # Javascript
-install_packages nodejs nodePackages.volar eslint_d prettierd
+nia nodejs nodePackages.volar eslint_d prettierd
 
 # Python
-install_packages python311Packages.jedi-language-server python311Packages.autopep8
+nia python311Packages.jedi-language-server python311Packages.autopep8
 
 # Markdown
-install_packages marksman
+nia marksman
 
 # Lua
-install_packages lua-language-server
+nia lua-language-server
 
 # Docker
-install_packages dockerfile-language-server-nodejs
+nia dockerfile-language-server-nodejs
 
 # Clean up garbage
 nix-store --gc
