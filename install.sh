a95dce1697b13067cf903ca244b2593969289cf7
Zsolt Horváth <hrvth.7slt@gmail.com>
2023-12-09 14:26:29 +0100

    install order

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 # install packages
 ./setup/nix.sh
 
+# symlink config and scripts
+./setup/link.sh
+
 # install keyd
 ./setup/keyd.sh
 
 # install kitty
 ./setup/kitty.sh
-
-# symlink config and scripts
-./setup/link.sh
