d70f799a1b58ecd565207c8ddfa5ab63a3d9003f
Zsolt Horváth <hrvth.7slt@gmail.com>
2023-12-30 13:49:14 +0100

    dap php

--- home/nvim/.config/nvim/lua/plugins/dap.lua
+++ home/nvim/.config/nvim/lua/plugins/dap.lua
@@ -34,17 +34,49 @@ local function go(dap)
     }
 end
 
+local function php(dap)
+    dap.adapters.php = {
+        type = "executable",
+        command = "php-debug-adapter",
+    }
+
+    dap.configurations.php = {
+        {
+            name = "Listen for Xdebug",
+            type = "php",
+            request = "launch",
+            port = 9000,
+            log = true
+        }
+    }
+end
+
 local function config()
     local dap = require('dap')
     local dapui = require('dapui')
 
     dapui.setup()
 
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+    end
+
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+    end
+
+    dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+    end
+
     go(dap)
+    php(dap)
+
+    require("nvim-dap-virtual-text").setup()
 
     vim.keymap.set('n', '<leader>bb', function() dap.toggle_breakpoint() end, { desc = 'deBug toggle Breakpoint' })
 
-    vim.keymap.set('n', '<leader>bt', function() dapui.toggle() end, { desc = 'deBug Toggle ui' })
+    vim.keymap.set('n', '<leader>bt', function() dapui.toggle({ reset = true }) end, { desc = 'deBug Toggle ui' })
     vim.keymap.set('n', '<leader>bb', function() dap.toggle_breakpoint() end, { desc = 'deBug toggle Breakpoint' })
     vim.keymap.set('n', '<leader>bc', function() dap.continue() end, { desc = 'deBug Continue' })
     vim.keymap.set('n', '<leader>bi', function() dap.step_into() end, { desc = 'deBug step Into' })
@@ -59,4 +91,5 @@ return {
         dependencies = "mfussenegger/nvim-dap",
         config = config,
     },
+    "theHamsta/nvim-dap-virtual-text"
 }
