fbd290227ef0b06767cba4b49280c598f3230335
Zsolt Horváth <hrvth.7slt@gmail.com>
2023-12-30 13:11:31 +0100

    zenbones

--- home/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ home/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,14 @@ local function config()
         let g:gruvbox_material_transparent_background=1
         let g:gruvbox_material_foreground='original'
         set background=light
-        colorscheme gruvbox-material
+        colorscheme zenbones
     ]])
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 return {
-    "sainnhe/gruvbox-material",
+    "mcchrish/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
     priority = 1000,
     config = config
 }
