109ff97d00dd660da20d9421e06d7205659b2f7e
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-01-01 20:51:58 +0100

    setup/keyd.sh

--- setup/keyd.sh
+++ setup/keyd.sh
@@ -8,7 +8,7 @@ fi
 git clone https://github.com/rvaiya/keyd keyd_repo
 cd keyd_repo || exit 1
 git checkout v2.4.3
-make && sudo make install
+make && sudo "$(command -v make)" install
 sudo systemctl enable keyd && sudo systemctl start keyd
 cd .. || exit 1
 rm -rf keyd_repo
