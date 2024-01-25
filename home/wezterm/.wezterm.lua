6510a359a91162b4bea338c9f99199e6e22e0b4f
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-01-25 09:53:13 +0100

    fix term env var

--- home/wezterm/.wezterm.lua
+++ home/wezterm/.wezterm.lua
@@ -11,7 +11,6 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.term = "wezterm"
 config.window_close_confirmation = "NeverPrompt"
 
 -- Hide tabs
