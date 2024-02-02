fb48832644a527df3a0cc342d1341a5705638877
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-02-02 16:16:00 +0100

    setup/nix.sh

--- setup/nix.sh
+++ setup/nix.sh
@@ -8,6 +8,11 @@ fi
 export NIXPKGS_ALLOW_UNFREE=1
 
 install_nix() {
+  if [ "$(which nix)" ]; then
+    echo "Nix is already installed"
+    return 0
+  fi
+
   sh <(curl -L https://nixos.org/nix/install) --no-daemon
   # shellcheck disable=1091
   . "$HOME"/.nix-profile/etc/profile.d/nix.sh
