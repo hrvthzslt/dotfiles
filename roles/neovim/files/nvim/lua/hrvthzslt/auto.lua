72198c611ad0ce09ab84acfd60e2bed7c613dd60
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-03-08 10:58:34 +0100

    feat(nvim): auto spell and wrap certain files

--- roles/neovim/files/nvim/lua/hrvthzslt/auto.lua
+++ roles/neovim/files/nvim/lua/hrvthzslt/auto.lua
@@ -8,3 +8,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+local wrap_spell_group = vim.api.nvim_create_augroup("WrapAndSpell", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	group = wrap_spell_group,
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
