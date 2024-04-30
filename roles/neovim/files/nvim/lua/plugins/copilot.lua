82e987fb7388731a194fd5a1456fd3d6300ec73d
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-04-30 12:07:29 +0200

    feat(nvim): copilot - dismiss

--- roles/neovim/files/nvim/lua/plugins/copilot.lua
+++ roles/neovim/files/nvim/lua/plugins/copilot.lua
@@ -10,6 +10,7 @@ local function config()
 	vim.keymap.set("i", "<C-j>", "copilot#Next()", { expr = true, silent = true })
 	vim.keymap.set("i", "<C-k>", "copilot#Previous()", { expr = true, silent = true })
 	vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+	vim.keymap.set("i", "<C-l>", "<Plug>(copilot-dismiss)")
 
 	vim.keymap.set("n", "<leader>cs", ":Copilot status<CR>", { desc = "Copilot Status" })
 	vim.keymap.set("n", "<leader>cp", ":Copilot panel<CR>", { desc = "Copilot Panel" })
