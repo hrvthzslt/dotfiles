376827453f5dd44f34f525343b338ed3e97ee0c1
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-05-15 15:17:09 +0200

    feat(nvim): oil keymaps

--- roles/neovim/files/nvim/lua/plugins/oil.lua
+++ roles/neovim/files/nvim/lua/plugins/oil.lua
@@ -10,6 +10,11 @@ local function config()
 			-- Show files and directories that start with "."
 			show_hidden = true,
 		},
+		keymaps = {
+			["<C-h>"] = false,
+			["<M-h>"] = "actions.select_split",
+			["<C-l>"] = false,
+		},
 	})
 	vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory" })
 end
