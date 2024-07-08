eb16004dee13039025ef05a548274d885312c69a
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-07-08 16:41:25 +0200

    feat(mnm): handling non linked folders

--- roles/neovim/files/nvim/lua/plugins/lsp_config.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp_config.lua
@@ -28,6 +28,10 @@ local function config()
 	setup("bash-language-server", function()
 		lspconfig.bashls.setup({
 			capabilities = capabilities,
+			on_attach = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end,
 		})
 	end)
 
