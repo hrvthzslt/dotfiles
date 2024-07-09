8caac517d2c7a66c0551bbf0d875c0e3895a0863
Zsolt Horváth <hrvth.7slt@gmail.com>
2024-07-09 09:51:40 +0200

    feat(nvim): djlint

--- roles/neovim/files/nvim/lua/plugins/null_ls.lua
+++ roles/neovim/files/nvim/lua/plugins/null_ls.lua
@@ -21,6 +21,8 @@ local function config()
 	)
 	-- setup("isort", sources, null_ls.builtins.formatting.isort)
 	setup("black", sources, null_ls.builtins.formatting.black)
+	setup("djlint", sources, null_ls.builtins.formatting.djlint)
+	setup("djlint", sources, null_ls.builtins.diagnostics.djlint)
 	setup(
 		"shfmt",
 		sources,
