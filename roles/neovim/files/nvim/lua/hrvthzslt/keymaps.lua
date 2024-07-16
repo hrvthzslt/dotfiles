055269124200f55ef5e36989d7f870019747010e
Zsolt Horvath <zsolt.horvath@byborgenterprises.com>
2024-07-16 13:59:11 +0200

    feat(nvim): remove unused keymaps

--- roles/neovim/files/nvim/lua/hrvthzslt/keymaps.lua
+++ roles/neovim/files/nvim/lua/hrvthzslt/keymaps.lua
@@ -17,18 +17,8 @@ vim.keymap.set("v", "y", "myy`y")
 -- Paste replace visual selection without copying it.
 vim.keymap.set("v", "p", '"_dP')
 
--- Easy insertion of a trailing ; or , from insert mode.
--- vim.keymap.set('i', ';;', '<Esc>A;')
--- vim.keymap.set('i', ',,', '<Esc>A,')
-
 -- Quickly clear search highlighting.
 vim.keymap.set("n", "<Leader>k", ":nohlsearch<CR>", { desc = "nohlsearch" })
-
--- Open the current file in the default program (on Mac this should just be just `open`).
-vim.keymap.set("n", "<Leader>x", ":!xdg-open %<CR><CR>", { desc = "Xdg open" })
-
--- Source file
-vim.keymap.set("n", "<Leader>S", ":so<CR>", { desc = "Source" })
 
 -- Copy file path
 vim.keymap.set("n", "<Leader>C", ":! echo % | xsel -ib<CR><CR>", { desc = "Copy file path" })
@@ -41,23 +31,9 @@ vim.keymap.set("n", "<A-k>", ":move .-2<CR>==")
 vim.keymap.set("v", "<A-j>", ":move '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-k>", ":move '<-2<CR>gv=gv")
 
--- Buffers
-vim.keymap.set("n", "<leader><leader>", ":bp<CR>", { desc = "previous buffer" })
-vim.keymap.set("n", "<leader>o", ":wincmd o<CR>", { desc = "focus on window" })
-
 -- Quickfix
 vim.keymap.set("n", "<leader>n", ":cnext<CR>", { desc = "Next quick fix item" })
 vim.keymap.set("n", "<leader>p", ":cprevious<CR>", { desc = "Previous quick fix item" })
-
--- Packer keymaps
--- vim.keymap.set("n", "<leader>ps", ":PackerSync<CR>", { desc = "Packer Sync" })
--- vim.keymap.set("n", "<leader>pc", ":PackerCompile<CR>", { desc = "Packer Compile" })
-
--- Navigation shortcuts
-vim.keymap.set("n", "ú", "}")
-vim.keymap.set("n", "ő", "{")
-vim.keymap.set("n", "Ú", "]m")
-vim.keymap.set("n", "Ő", "[m")
 
 -- Registers
 -- Mapping for "c" and "C"
