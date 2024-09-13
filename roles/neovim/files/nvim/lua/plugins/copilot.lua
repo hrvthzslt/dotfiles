de54f9039c90555a19aec953cb6ae43466246911
Zsolt Horvath <zsolt.horvath@byborgenterprises.com>
2024-09-13 17:38:37 +0200

    feat(nvim): new copilot plugin and avante

--- roles/neovim/files/nvim/lua/plugins/copilot.lua
+++ roles/neovim/files/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,4 @@
-local function config()
+local function old_config()
 	vim.g.copilot_no_tab_map = true
 	vim.g.copilot_assume_mapped = true
 	vim.b.copilot_enabled = true
@@ -18,6 +18,41 @@ local function config()
 	vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = "Copilot Disable" })
 end
 
+-- return {
+-- 	{ "github/copilot.vim", config = config },
+-- }
+
+local function config()
+	require("copilot").setup({
+		panel = {
+			enabled = false,
+		},
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+			hide_during_completion = true,
+			debounce = 75,
+			keymap = {
+				accept = "<C-h>",
+				accept_word = false,
+				accept_line = false,
+				next = "<C-j>",
+				prev = "<C-k>",
+				dismiss = "<C-l>",
+			},
+		},
+		filetypes = {},
+		copilot_node_command = "node", -- Node.js version must be > 18.x
+		server_opts_overrides = {},
+	})
+
+	vim.keymap.set("n", "<leader>cs", ":Copilot status<CR>", { desc = "Copilot Status" })
+	vim.keymap.set("n", "<leader>cp", ":Copilot panel<CR>", { desc = "Copilot Panel" })
+	vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { desc = "Copilot Enabe" })
+	vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = "Copilot Disable" })
+end
+
 return {
-	{ "github/copilot.vim", config = config },
+	{ "zbirenbaum/copilot.lua", config = config },
+	{ "AndreM222/copilot-lualine" },
 }
