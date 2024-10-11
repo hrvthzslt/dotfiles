058ff546ff33488458e9977c10d6a96e371c0c64
Zsolt Horvath <hrvth.7slt@gmail.com>
2024-10-11 15:29:50 +0200

    feat(nvim): markdown preview settings

--- roles/neovim/files/nvim/lua/plugins/markdown-preview.lua
+++ roles/neovim/files/nvim/lua/plugins/markdown-preview.lua
@@ -1,15 +1,16 @@
 local function config()
+	vim.g.mkdp_refresh_slow = 1
+	vim.g.mkdp_theme = "light"
+	vim.g.mkdp_filetypes = { "markdown" }
+
 	vim.keymap.set("n", "mm", ":MarkdownPreview<CR>", { desc = "Markdown preview" })
-    vim.keymap.set("n", "ms", ":MarkdownPreviewStop<CR>", { desc = "Markdown Stop" })
+	vim.keymap.set("n", "ms", ":MarkdownPreviewStop<CR>", { desc = "Markdown Stop" })
 end
 
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
 	ft = { "markdown" },
 	config = config,
 }
