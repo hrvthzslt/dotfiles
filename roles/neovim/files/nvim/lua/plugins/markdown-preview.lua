61d1a03c368a8e416db17c67f8542aac03a8716a
Zsolt Horvath <hrvth.7slt@gmail.com>
2024-10-21 13:01:11 +0200

    feat(nvim): markdown previw config

--- roles/neovim/files/nvim/lua/plugins/markdown-preview.lua
+++ roles/neovim/files/nvim/lua/plugins/markdown-preview.lua
@@ -2,7 +2,9 @@ local function config()
 	vim.g.mkdp_refresh_slow = 1
 	vim.g.mkdp_theme = "light"
 	vim.g.mkdp_filetypes = { "markdown" }
-
+	-- vim.g.mkdp_preview_options = {
+	-- 	uml = { server = "http://localhost:8111" },
+	-- }
 	vim.keymap.set("n", "mm", ":MarkdownPreview<CR>", { desc = "Markdown preview" })
 	vim.keymap.set("n", "ms", ":MarkdownPreviewStop<CR>", { desc = "Markdown Stop" })
 end
@@ -10,7 +12,7 @@ end
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
+	build = "cd app && npx --yes yarn install",
 	ft = { "markdown" },
 	config = config,
 }
