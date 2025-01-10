f7748ee5b07f1eb28fe7110ca81b82bdd7e19601
Zsolt Horvath <zsolt.horvath@byborgenterprises.com>
2025-01-10 10:18:10 +0100

    feat(nvim): remove unused plugins (#15)

--- roles/neovim/files/nvim/lua/plugins/editor.lua
+++ roles/neovim/files/nvim/lua/plugins/editor.lua
@@ -1,19 +1,8 @@
-local function splitjoin()
-	vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-	vim.g.splitjoin_trailing_comma = 1
-	vim.g.splitjoin_php_method_chain_full = 1
-end
-
 local function vim_pasta()
 	vim.g.pasta_disabled_filetypes = { "fugitive" }
 end
 
 return {
-	-- Split arrays and methods onto multiple lines, or join them back up.
-	{
-		"AndrewRadev/splitjoin.vim",
-		config = splitjoin,
-	},
 	-- Automatically fix indentation when pasting code.
 	{
 		"sickill/vim-pasta",
@@ -21,18 +10,12 @@ return {
 	},
 	-- Commenting support.
 	{ "tpope/vim-commentary" },
-	-- Useful commands like :Rename and :SudoWrite.
-	{ "tpope/vim-eunuch" },
 	-- Allow plugins to enable repeating of commands.
 	{ "tpope/vim-repeat" },
 	-- Surround text objects with brackets, quotes, etc.
 	{ "tpope/vim-surround" },
 	-- Jump to the last location when opening a file.
 	{ "farmergreg/vim-lastplace" },
-	-- Enable * searching with visually selected text.
-	{ "nelstrom/vim-visual-star-search" },
-	-- Automatically create parent dirs when saving.
-	{ "jessarcher/vim-heritage" },
 	-- Automatically add closing brackets, quotes, etc.
 	{
 		"windwp/nvim-autopairs",
