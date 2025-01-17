e660b6f1f1f14bca0f887332009065248eb02785
Zsolt Horvath <zsolt.horvath@byborgenterprises.com>
2025-01-17 15:40:12 +0100

    feat(nvim): renovate cmp config

--- roles/neovim/files/nvim/lua/plugins/dadbod.lua
+++ roles/neovim/files/nvim/lua/plugins/dadbod.lua
@@ -1,34 +1,11 @@
 local function config()
 	vim.keymap.set("n", "<leader>d", ":DBUIToggle<CR>", { desc = "Database" })
-
-	local function db_completion()
-		require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-	end
-
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = {
-			"sql",
-		},
-		command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
-	})
-
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = {
-			"sql",
-			"mysql",
-			"plsql",
-		},
-		callback = function()
-			vim.schedule(db_completion)
-		end,
-	})
 end
 
 return {
-	"tpope/vim-dadbod",
+	"kristijanhusak/vim-dadbod-ui",
 	dependencies = {
-		"kristijanhusak/vim-dadbod-ui",
-		"kristijanhusak/vim-dadbod-completion",
+		"tpope/vim-dadbod",
 	},
 	config = config,
 }
