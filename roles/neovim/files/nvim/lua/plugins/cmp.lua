13174cab057c1fe72499c68eb4913ec9036176df
Zsolt Horvath <zsolt.horvath@byborgenterprises.com>
2025-01-17 15:44:01 +0100

    feat(nvim): renovate cmp config (#16)

--- roles/neovim/files/nvim/lua/plugins/cmp.lua
+++ roles/neovim/files/nvim/lua/plugins/cmp.lua
@@ -1,14 +1,10 @@
 local function config()
+	local lspkind = require("lspkind")
+
 	local cmp = require("cmp")
 
 	require("luasnip.loaders.from_vscode").lazy_load()
 	local luasnip = require("luasnip")
-
-	local has_words_before = function()
-		unpack = unpack or table.unpack
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
 
 	cmp.setup({
 		preselect = "item",
@@ -16,46 +12,32 @@ local function config()
 			completeopt = "menu,menuone,noinsert",
 		},
 		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-o>"] = cmp.mapping.complete(),
 			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			["<C-n>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-					-- they way you will only jump inside the snippet region
-				elseif luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				elseif has_words_before() then
-					cmp.complete()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-
-			["<C-p>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+			["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+			["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 		}),
-		---@diagnostic disable-next-line: undefined-field
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp" },
-			{ name = "luasnip" },
 			{ name = "nvim_lsp_signature_help" },
+			{ name = "luasnip" },
 		}, {
 			{ name = "buffer" },
 		}),
 		snippet = {
 			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
+				luasnip.lsp_expand(args.body)
 			end,
+		},
+		formatting = {
+			format = lspkind.cmp_format(),
+		},
+	})
+
+	cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
+		sources = {
+			{ name = "vim-dadbod-completion" },
+			{ name = "buffer" },
 		},
 	})
 end
@@ -63,12 +45,14 @@ end
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
+		"onsails/lspkind-nvim",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"hrsh7th/cmp-buffer",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
+		"kristijanhusak/vim-dadbod-completion",
 	},
 	config = config,
 }
