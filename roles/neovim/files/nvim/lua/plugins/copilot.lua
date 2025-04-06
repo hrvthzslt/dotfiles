e63996baecfcc899114727ae1c2134e092a2ceff
Zsolt Horváth <hrvth.7slt@gmail.com>
2025-04-06 13:21:30 +0200

    feat(nvim): copilot - disable autotrigger

--- roles/neovim/files/nvim/lua/plugins/copilot.lua
+++ roles/neovim/files/nvim/lua/plugins/copilot.lua
@@ -1,27 +1,3 @@
-local function old_config()
-	vim.g.copilot_no_tab_map = true
-	vim.g.copilot_assume_mapped = true
-	vim.b.copilot_enabled = true
-	vim.g.copilot_filetypes = {
-		markdown = true,
-		yaml = true,
-	}
-
-	vim.keymap.set("i", "<C-j>", "copilot#Next()", { expr = true, silent = true })
-	vim.keymap.set("i", "<C-k>", "copilot#Previous()", { expr = true, silent = true })
-	vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-	vim.keymap.set("i", "<C-l>", "<Plug>(copilot-dismiss)")
-
-	vim.keymap.set("n", "<leader>cs", ":Copilot status<CR>", { desc = "Copilot Status" })
-	vim.keymap.set("n", "<leader>cp", ":Copilot panel<CR>", { desc = "Copilot Panel" })
-	vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { desc = "Copilot Enabe" })
-	vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = "Copilot Disable" })
-end
-
--- return {
--- 	{ "github/copilot.vim", config = config },
--- }
-
 local function config()
 	require("copilot").setup({
 		panel = {
@@ -29,7 +5,7 @@ local function config()
 		},
 		suggestion = {
 			enabled = true,
-			auto_trigger = true,
+			auto_trigger = false,
 			hide_during_completion = true,
 			debounce = 75,
 			keymap = {
